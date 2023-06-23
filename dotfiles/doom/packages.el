27437613350f8e87d3e41965f0a3f0dc6893e776
Juozas Norkus <jnorkus@evolution.com>
2023-06-23 11:21:53 +0100

    Upgrade elixir-mode for ~H support

--- dotfiles/doom/packages.el
+++ dotfiles/doom/packages.el
@@ -4,3 +4,6 @@
 (package! groovy-mode)
 (package! rfc-mode)
 (package! chatgpt-shell)
+
+;; I need ~H sigil support, need to run doom sync -u for this to take into effect
+(package! elixir-mode :pin "6c7f2b2b358919c6a3dd857c9de66d32ca6f9a8c")
