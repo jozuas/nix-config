62726f923f016c4a78e0262b33070decf283141b
Juozas Norkus <jnorkus@evolution.com>
2023-06-23 10:52:27 +0100

    Update MacOS

--- home-manager/environment/common.nix
+++ home-manager/environment/common.nix
@@ -32,7 +32,7 @@ in {
     nmap
     sshpass
 
-    # Doom, Vim
+    # IDE tools
     emacs-all-the-icons-fonts
     python3Packages.pytest
     python3Packages.flake8
@@ -40,6 +40,8 @@ in {
     python3Packages.isort
     nodePackages.pyright
     nodePackages.bash-language-server
+    shellcheck
+    shfmt
     nodePackages.vscode-langservers-extracted
     nodePackages.yaml-language-server
     nodePackages.typescript-language-server
@@ -49,24 +51,20 @@ in {
     elvis-erlang
     erlfmt
     erlang-ls
-    pkgs_unstable.elixir_ls
-    rnix-lsp
+    elixir_ls
     nixfmt
-    nix-linter
+    pkgs_unstable.nixd
+    rustfmt
+    rust-analyzer
     hunspell
     hunspellDicts.en-gb-large
     proselint
     ripgrep
     coreutils
-    rustfmt
-    rust-analyzer
 
     # Programming language tooling
     gnumake
     sqlite
-    ## Bash
-    shellcheck
-    shfmt
     ## Python
     python3
     ## JavaScript
@@ -76,7 +74,6 @@ in {
     pkgs_unstable.elixir
     pkgs_unstable.erlangR25
     rebar3
-    rebar
     ## Rust
     rustc
     cargo
