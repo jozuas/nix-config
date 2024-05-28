f13bf3f8867edd5c717c6d8984c344edf1ddc408
Juozas Norkus <norkus@norkus.net>
2024-05-28 22:26:11 +0100

    Add devenv cachix substituters

--- nix-darwin/darwin-configuration.nix
+++ nix-darwin/darwin-configuration.nix
@@ -18,9 +18,13 @@ in {
       auto-optimise-store = true; # Run nix-store --optimise
 
       # Binary caches
-      substituters = [ "https://nix-community.cachix.org" ];
+      substituters = [
+        "https://nix-community.cachix.org" 
+        "https://devenv.cachix.org"
+      ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       ];
     };
 
