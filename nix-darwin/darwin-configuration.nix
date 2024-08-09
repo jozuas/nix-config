874cd240d7202f06b8f86afa617fa1d1262cc26b
Juozas Norkus <norkus@norkus.net>
2024-08-09 16:25:21 +0300

    Add trusted users

--- nix-darwin/darwin-configuration.nix
+++ nix-darwin/darwin-configuration.nix
@@ -17,12 +17,13 @@ in {
     settings = {
       auto-optimise-store = true; # Run nix-store --optimise
 
+      trusted-users = [ "root" "juozas" ];
+
       # Binary caches
       substituters =
-        [ "https://nix-community.cachix.org" "https://devenv.cachix.org" ];
+        [ "https://nix-community.cachix.org" ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       ];
     };
 
