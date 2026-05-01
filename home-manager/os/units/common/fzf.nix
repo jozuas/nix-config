ed7ad10dfe12032d701df4ed4ca0023b0d4b6f60
Juozas Norkus <norkus@norkus.net>
2026-05-01 21:18:27 +0100

    Small formatting

--- home-manager/os/units/common/fzf.nix
+++ home-manager/os/units/common/fzf.nix
@@ -1,10 +1,13 @@
 { pkgs, ... }:
 
-let fdExclude = "--exclude .git --exclude node_modules --exclude result";
-in {
+let
+  fdExclude = "--exclude .git --exclude node_modules --exclude result";
+in
+{
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
+    enableBashIntegration = true;
     defaultCommand = "${pkgs.fd}/bin/fd --type f ${fdExclude}";
     defaultOptions = [
       "--exact"
@@ -18,8 +21,7 @@ in {
       "--bind ctrl-p:toggle-preview,alt-k:preview-up,alt-j:preview-down"
     ];
     changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type d ${fdExclude}";
-    changeDirWidgetOptions =
-      [ "--preview '${pkgs.tree}/bin/tree -C {} | head -200'" ];
+    changeDirWidgetOptions = [ "--preview '${pkgs.tree}/bin/tree -C {} | head -200'" ];
     fileWidgetCommand = "${pkgs.fd}/bin/fd --type f ${fdExclude}";
     fileWidgetOptions = [
       "--preview '(${pkgs.bat}/bin/bat --color=always {} || cat {}) 2> /dev/null'"
